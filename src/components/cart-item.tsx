63fcbea730046821c4aa3e63b6f0788919f1c4ac
Awaddd <34748292+Awaddd@users.noreply.github.com>
2024-12-20 22:31:08 +0000

    Improve ui

--- src/components/cart-item.tsx
+++ src/components/cart-item.tsx
@@ -16,13 +16,13 @@ export default function ({ item, action = "add" }: Props) {
     }
 
     const AddButton = (
-        <button class="text-gray-400" onClick={addToCart}>
+        <button class="text-blue-400" onClick={addToCart}>
             Add
         </button>
     );
 
     const RemoveButton = (
-        <button class="text-gray-400" onClick={removeFromCart}>
+        <button class="text-red-400" onClick={removeFromCart}>
             Remove
         </button>
     );
@@ -30,9 +30,16 @@ export default function ({ item, action = "add" }: Props) {
     return (
         <div class="p-3 border border-gray-600 rounded flex justify-between">
             <div class="flex flex-col gap-1.5">
-                <span class="text-gray-400">{item.name}</span>
+                <div class="text-gray-300">
+                    {item.name}
+                    {"count" in item ? (
+                        <span>
+                            &nbsp;-&nbsp;
+                            <span class="text-gray-400">{item.count}x</span>
+                        </span>
+                    ) : <span />}
+                </div>
                 <span class="text-yellow-400">£{item.price}</span>
-                {"count" in item ? <span>{item.count}</span> : <span />}
             </div>
             {action === "add" ? AddButton : RemoveButton}
         </div>
